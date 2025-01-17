450aba7eb93852edd68cb998f45c8b69c2ae2c7b
Arch Sib <dmitash06@gmail.com>
2025-01-17 12:08:33 +0700

    Добавлено LB4 и LB5.

---
+++ LB_5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LB_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            string str = (string)listBox1.Items[index];
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+
+                if ((c >= 'а' && c <= 'я') || c == 'ё')
+                {
+                    count++;
+                }
+            }
+            label1.Text = $"Количество строчных букв = {count}";
+        }
+    }
+}
